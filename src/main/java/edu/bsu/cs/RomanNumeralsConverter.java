589629a6a8c754e65bfcd70dd8e443e23121fd3a
battman2305 <carson@test.soliddesign.net>
2024-01-23 11:58:34 -0500

    Step 2: advanced tests with full functionality, may need refactoring

--- src/main/java/edu/bsu/cs/RomanNumeralsConverter.java
+++ src/main/java/edu/bsu/cs/RomanNumeralsConverter.java
@@ -2,18 +2,30 @@ package edu.bsu.cs;
 
 public class RomanNumeralsConverter {
     public String convert(int n){
-        if((n - 1000) >= 0){
-            return "M" + convert(n-1000);
+        if((n - 1000) >= 0) {
+            return "M" + convert(n - 1000);
+        }else if((n - 900) >= 0){
+            return "CM" + convert(n-900);
         }else if((n - 500) >= 0){
             return "D" + convert(n-500);
+        }else if((n - 400) >= 0){
+            return "CD" + convert(n-400);
         }else if((n - 100) >= 0){
             return "C" + convert(n-100);
+        }else if((n - 90) >= 0){
+            return "XC" + convert(n-90);
         }else if((n - 50) >= 0){
             return "L" + convert(n-50);
+        }else if((n - 40) >= 0){
+            return "XL" + convert(n-40);
         }else if((n - 10) >= 0){
             return "X" + convert(n-10);
+        }else if((n - 9) >= 0){
+            return "IX" + convert(n-9);
         }else if((n - 5) >= 0){
             return "V" + convert(n-5);
+        }else if((n - 4) >= 0){
+            return "IV" + convert(n-4);
         }else if((n - 1) >= 0){
             return "I" + convert(n-1);
         }
--- src/test/java/edu/bsu/cs/RomanNumeralsTest.java
+++ src/test/java/edu/bsu/cs/RomanNumeralsTest.java
@@ -32,4 +32,32 @@ public class RomanNumeralsTest {
         final String output = numberConverter.convert(6);
         Assertions.assertEquals("VI", output);
     }
+
+    @Test
+    public void testRomanNumerals1234(){
+        final RomanNumeralsConverter numberConverter= new RomanNumeralsConverter();
+        final String output = numberConverter.convert(1234);
+        Assertions.assertEquals("MCCXXXIV", output);
+    }
+
+    @Test
+    public void testRomanNumerals999(){
+        final RomanNumeralsConverter numberConverter= new RomanNumeralsConverter();
+        final String output = numberConverter.convert(999);
+        Assertions.assertEquals("CMXCIX", output);
+    }
+
+    @Test
+    public void testRomanNumerals3000(){
+        final RomanNumeralsConverter numberConverter= new RomanNumeralsConverter();
+        final String output = numberConverter.convert(3000);
+        Assertions.assertEquals("MMM", output);
+    }
+
+    @Test
+    public void testRomanNumerals1111(){
+        final RomanNumeralsConverter numberConverter= new RomanNumeralsConverter();
+        final String output = numberConverter.convert(1111);
+        Assertions.assertEquals("MCXI", output);
+    }
 }
